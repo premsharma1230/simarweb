66c41f83a2ade044063b7790af2edaf3a7b5ff96
premsharma1230 <60520468+premsharma1230@users.noreply.github.com>
2021-11-07 13:41:32 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
+// $(document).ready(function () {
+//   $(".main-bar").click(function (e) {
+//     $(this).toggleClass("activefile");
+//     e.preventDefault();
+//     $(".Nav_Wrapper").slideToggle();
+//   });
+//   $(".slider").owlCarousel({
+//     loop: true,
+//     margin: 0,
+//     autoplay: false,
+//     nav: true,
+//     autoplayTimeout: 4000,
+//     smartSpeed: 1500,
+//     responsive: {
+//       0: {
+//         items: 1,
+//       },
+//       600: {
+//         items: 1,
+//       },
+//       1000: {
+//         items: 1,
+//       },
+//     },
+//   });
+// });
+
 $(document).ready(function () {
-  $(".slider1").owlCarousel({
-    loop: false,
-    margin: 0,
-    autoplay: true,
-    // nav: true,
-    autoplayTimeout: 4000,
-    smartSpeed: 1500,
-    animateOut: "fadeOut",
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 1,
-      },
-    },
+  $("button").click(function () {
+    $("p").hide();
   });
 });
